b1d68dae2800444e37b818e89e3dabadfec59fdd
Carlos Madrazo <madrazo@bamtang.com>
2018-10-09 23:20:17 -0500

    small cleanup

--- samples/Blur/Source/Main.hx
+++ samples/Blur/Source/Main.hx
@@ -9,7 +9,6 @@ import nme.events.Event;
 
 import go.effects.Blur;
 
-
 class Main extends Sprite {
 
     var mBlurNode:Blur;
@@ -19,19 +18,15 @@ class Main extends Sprite {
         
         super ();
 
+        var logo = new Bitmap ( Assets.getBitmapData ( "assets/nme.png" ) );
+        logo.x =  ( stage.stageWidth - logo.width ) / 2;
+        logo.y =  ( stage.stageHeight - logo.height ) / 2;
+
         //target sizes
         var width = stage.stageWidth;
         var height = stage.stageWidth;
         var scale:Float = 1.0/8.0;  //blur using smaller render targets
-        var widthScaled = Std.int(width*scale);
-        var heightScaled = Std.int(height*scale);
-
-        var data = Assets.getBitmapData ( "assets/nme.png" ) ;
-        var logo = new Bitmap ( data );
-        logo.x =  ( stage.stageWidth - logo.width ) / 2;
-        logo.y =  ( stage.stageHeight - logo.height ) / 2;
-
-        mBlurNode = new Blur(stage.stageWidth, stage.stageHeight, scale);
+        mBlurNode = new Blur(width, height, scale);
 
         addChild(mBlurNode);
         mBlurNode.addChild(logo);
